00e8c0da75477aea391c23328e206ba39b7519cc
Sakumzi <skenjanakeith@gmail.com>
2023-03-25 09:26:58 -0700

    Tasks

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    '''amenuty class'''
+    '''amenity class'''
     __tablename__ = 'amenities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
--- models/user.py
+++ models/user.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
+        places = relationship("Place", passive_deletes=True, backref="user")
+        reviews = relationship("Review", passive_deletes=True, backref="user")
     else:
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
